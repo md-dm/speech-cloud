eca70a17f084301d44888b694bf76b2a6b4c6510
Diego Martín Correa <diegomartincorrea@gmail.com>
2012-11-16 12:57:29 -0300

    Adding server side process over speech files

---
+++ process/process_files.py
@@ -0,0 +1,89 @@
+	
+import os
+import re
+import codecs
+from collections import Counter, defaultdict
+import pymongo
+from pymongo import Connection
+import datetime
+
+
+#######################################################################
+## MAIN
+# Start the process
+
+#mongo ds043057.mongolab.com:43057/datafest -u <dbuser> -p <dbpassword>
+
+connection = Connection('ds043027.mongolab.com', 43027)
+db = connection['datafest']
+db.authenticate("datafest", "mddm")
+cloud = db['cloud']
+
+print cloud.count()
+
+
+totalCounter = Counter()
+
+for filename in os.listdir("Discursos-Presidenciales"):
+
+	file = codecs.open("Discursos-Presidenciales/" + filename, "r", "iso-8859-1")
+	#print file.read().lower()
+
+	wordsCount = re.findall('\w+', file.read().lower())
+	
+	print wordsCount 
+	#print wordsCount
+	words_cloud = {}
+	words_cloud["filename"] = filename
+	
+	counter = Counter(wordsCount)
+	print counter
+	dictionary = ["a", "e", "i", "o", "u", "y", "el", "en", "la", "de", "que", "con", "un", "las", "los", "ese", "es", "eso", "esa", "esos", "la", "los", "del", "ha", "esto", "para", "por", "muy", "lo", "hay", "son", "nos", "sido", "sus", "pero", "ah"]
+
+	for word in dictionary:
+		del counter[word]
+
+
+	words = []
+	
+	totalCounter += counter
+
+	for k, v in counter.iteritems():
+		#print k
+		words.append({"text":k, "size":v})
+
+	words_cloud["words"] = words 
+
+
+	words_cloud["date"] = datetime.datetime.strptime(filename[0:8], "%Y%m%d")
+	words_cloud["sdate"] = filename[0:8]
+
+	print words_cloud
+	#cloud.insert(words_cloud)
+
+
+
+#Total Words!
+words_cloud = {}
+words_cloud["filename"] = "20121230 Palabras totales.TXT"
+
+for k, v in counter.iteritems():
+	#print k
+	words.append({"text":k, "size":v})
+
+	words_cloud["words"] = words 
+
+
+	words_cloud["date"] = datetime.datetime.strptime(filename[0:8], "%Y%m%d")
+	words_cloud["sdate"] = filename[0:8]
+
+
+#print words_cloud
+cloud.insert(words_cloud)
+
+
+
+
+
+
+
